5197467f3a97054ce422f9ac24f532fd45e8fcb6
David Borde <dborde@gogoair.com>
2019-01-31 21:13:23 -0600

    adding animation to polyline.  Adding svg icons to images

---
+++ CHANGELOG.md
@@ -0,0 +1,10 @@
+# Flight Tracker React
+
+## v.1.0.0
+* *Feature*: adding animation to polyline.  Adding svg icons to images
+* *Feature*: addWaypoints method
+* *Feature*: adding GreatCircleArc poc
+* *Fix*: removing notes folder
+* *Feature*: adding notes to gitiignore
+* *Fix*: re-init yarn
+* *Feature*: Initial commit
--- src/components/FlightTrackerPage.js
+++ src/components/FlightTrackerPage.js
@@ -56,7 +56,7 @@ export class FlightTrackerPage extends React.Component {
     this.state = {
       waypoints: [],
       markers: [],
-      planePath: [],
+      // planePath: [],
       currentZoomLevel: zoomLevel
     };
   }
@@ -69,13 +69,33 @@ export class FlightTrackerPage extends React.Component {
     this.setState({ markers });
   }
 
-  addWaypoints = (previous, current, v = 200, o = 100) => {
+  /*
+  * The vertices 'v' argument to Arc specifies the number of intermediate
+  * vertices you want in the resulting line. The higher the number
+  * the more dense and accurate the line will be.
+  *
+  * The offset 'o' argument controls the likelyhood that lines will be split
+  * which cross the dateline. The higher the number the more likely.
+  * The default value is 10, which means lines within 10 degress
+  * of the dateline will be split. For lines that cross and dateline
+  * and are also near the poles you will likely need a higher value
+  * to trigger splitting. It is unclear to me (@springmeyer) what the
+  * drawbacks are of high offsets. I simply ported the code from OGR's
+  * [gdal/ogr/ogrgeometryfactory.cpp](https://github.com/OSGeo/gdal/blob/master/gdal/ogr/ogrgeometryfactory.cpp)
+  * and have not taken the time to fully comprehend how it works.
+  */
+  addWaypoints = (previous, current, v = 200, o = 10) => {
     const {
       waypoints
     } = this.state;
     const data = L.Polyline.Arc(previous, current, { vertices: v, offset: o });
-    data._latlngs.forEach((x) => waypoints.push(x));
-    this.setState({ waypoints });
+    data._latlngs.forEach((x, index) => {
+      // could animation of line get any simpler than this?
+      setTimeout(() => {
+        waypoints.push(x);
+        this.setState({ waypoints });
+      }, 3 * index);
+    });
   }
 
   componentDidMount() {
