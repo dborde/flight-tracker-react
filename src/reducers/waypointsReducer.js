9e5d3ca588837b6f572b30ad7375f0e1f6b6f264
David Borde <dborde@gogoair.com>
2019-03-26 21:07:45 -0500

    remove TODO remove duplicate latLngs: doubly nested

--- src/reducers/waypointsReducer.js
+++ src/reducers/waypointsReducer.js
@@ -3,7 +3,6 @@ const waypointsReducerDefaultState = [];
 export default (state = waypointsReducerDefaultState, action) => {
   switch (action.type) {
     case 'ADD_WAYPOINTS':
-      // TODO remove duplicate latLngs: doubly nested
       return [
         ...state,
         action.waypoints
